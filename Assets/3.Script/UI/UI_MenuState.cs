6bb43bb225e28f05427beed58bcd472cdf3caaac
goodeun <goodeun10.1@gmail.com>
2026-09-21 17:27:48 +0900

    .

--- Assets/3.Script/UI/UI_MenuState.cs
+++ Assets/3.Script/UI/UI_MenuState.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using DG.Tweening;
 
 public class UI_MenuState : MonoBehaviour
 {
